93b22a3a8adfa8a7ccaccbb7567ebc072e3f399c
ejaay-dev <ejaay.dev@gmail.com>
2024-12-18 20:05:56 +0800

    Fourth commit Update on json-server file

--- api/json-server.js
+++ api/json-server.js
@@ -21,7 +21,7 @@ server.use(middlewares)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
+    // "/task/:resource/:id/show": "/:resource/:id",
   })
 )
 server.use(router)
