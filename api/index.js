6dbf3cc3c4692fa78cb50d6891c7bfc0ba3c8b0e
ejaay-dev <ejaay.dev@gmail.com>
2024-12-23 22:11:53 +0800

    Eleventh changes Revert the changes in the index.js file Deleted the tmp directory Updated the tasks array data

--- api/index.js
+++ api/index.js
@@ -2,9 +2,7 @@ const jsonServer = require("json-server")
 const path = require("path")
 
 const server = jsonServer.create()
-
-// Created a tmp folder and placed the db.json to solve EROFS issue
-const router = jsonServer.router(path.join(__dirname, "tmp", "db.json"))
+const router = jsonServer.router(path.join(__dirname, "../db.json"))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
